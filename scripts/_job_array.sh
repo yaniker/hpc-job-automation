286e15f75f9e9ca3ad99298b268bf7d55b32a422
Erim Yanik <erimyanik@wc-dhcp3d074.eduroam.wireless.fsu.edu>
2025-03-21 12:37:12 -0400

    Added _params.txt file check

--- scripts/_job_array.sh
+++ scripts/_job_array.sh
@@ -14,6 +14,11 @@ if [ -n "$ENV_ACTIVATE" ]; then
     eval "$ENV_ACTIVATE"
 fi
 
+if [ ! -f "_params.txt" ]; then
+    echo "Error: _params.txt does not exist" >&2
+    exit 1
+fi
+
 # Read parameters for this task from _params.txt
 PARAMS=$(sed -n "${SLURM_ARRAY_TASK_ID}p" _params.txt)  # Do NOT change the name
 
